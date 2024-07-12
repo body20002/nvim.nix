b0d80c4d3a792a171d298e9bd1741ac15087a98b
body20002 <body20002.test@gmail.com>
2024-07-12 04:25:38 +0300

    feat(lualine): stylized

--- config/plugins/ui/lualine.nix
+++ config/plugins/ui/lualine.nix
@@ -11,5 +11,65 @@ _: {
     };
     alwaysDivideMiddle = true;
 
+    sections = {
+      lualine_a = [ "mode" ];
+      lualine_b = [
+        "branch"
+        "diff"
+        {
+          name = "diagnostics";
+          extraConfig = {
+            sources = [ "nvim_lsp" "coc" ];
+            update_in_insert = true;
+            always_visible = true;
+          };
+        }
+      ];
+
+      lualine_c = [
+        {
+          name = "filename";
+          extraConfig = {
+            file_status = true;
+            path = 1;
+          };
+        }
+      ];
+      lualine_x = [
+        {
+          name = ''vim.fn["codeium#GetStatusString"]()'';
+          fmt = ''
+            function(str)
+              local trimmed = str:match("^%s*(.-)%s*$")
+              if trimmed == "OFF" then return "󱚡 " end
+              if trimmed == "ON" then return "󰚩 " end
+              if trimmed == "*" then return "󱚣 " end
+              return "󰚩  " .. trimmed
+            end
+          '';
+        }
+        "filetype"
+      ];
+      lualine_y = [ "%p%%" "location" ];
+      lualine_z = null;
+    };
+    inactiveSections = {
+      lualine_a = null;
+      lualine_b = null;
+      lualine_c = [
+        {
+          name = "filename";
+          extraConfig = {
+            file_status = true;
+            path = 1;
+          };
+        }
+      ];
+      lualine_x = [
+        "filetype"
+      ];
+      lualine_y = [ "%p%%" "location" ];
+      lualine_z = null;
+    };
   };
 }
