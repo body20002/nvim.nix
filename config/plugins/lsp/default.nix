2904eef18927c6f1ad9e245bdffb0b0f3a69e5fb
body20002 <body20002.test@gmail.com>
2024-09-30 00:32:02 +0300

    update: switch to eslint_d

--- config/plugins/lsp/default.nix
+++ config/plugins/lsp/default.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     ./coq.nix
     ./lintting.nix
@@ -23,7 +23,10 @@
         docker-compose-language-service.enable = true;
         dockerls.enable = true;
         # emmet-ls.enable = true;
-        eslint.enable = true;
+        eslint = {
+          enable = true;
+          package = pkgs.eslint_d;
+        };
         gopls.enable = true;
         # htmx.enable = true;
         jsonls.enable = true;
