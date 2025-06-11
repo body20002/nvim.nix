6d2b4ecfe8243de65a1ed834869a7e1a7df23260
body20002 <body20002.test@gmail.com>
2025-06-11 16:41:13 +0300

    chore: update to new lsp config

--- config/plugins/lsp/default.nix
+++ config/plugins/lsp/default.nix
@@ -4,72 +4,85 @@
     ./autocomplete.nix
   ];
 
-  plugins = {
-    lsp = {
-      enable = true;
-      keymaps.lspBuf = {
-        gD = "references";
-        gd = "definition";
-        gi = "implementation";
-        gt = "type_definition";
-        K = "hover";
-        "<leader>k" = "signature_help";
-        "<leader>F" = "format";
-      };
-      servers = {
-        "*" = {
-          settings = {
-            capabilities = {
-              textDocument = {
-                semanticTokens = {
-                  multilineTokenSupport = true;
-                };
+  lsp = {
+    keymaps.lspBuf = {
+      gD = "references";
+      gd = "definition";
+      gi = "implementation";
+      gt = "type_definition";
+      K = "hover";
+      "<leader>k" = "signature_help";
+      "<leader>F" = "format";
+    };
+    servers = {
+      "*" = {
+        settings = {
+          capabilities = {
+            textDocument = {
+              semanticTokens = {
+                multilineTokenSupport = true;
               };
             };
-            root_markers = [
-              ".git"
-            ];
           };
+          root_markers = [
+            ".git"
+          ];
         };
-        bashls.enable = true;
-        biome.enable = true;
-        clangd.enable = true;
-        docker_compose_language_service.enable = true;
-        dockerls.enable = true;
-        gopls.enable = true;
-        kotlin_language_server.enable = true;
-        lua_ls.enable = true;
-        marksman = {
-          enable = true;
-          settings = {
-            filetypes = ["md" "markdown"];
-          };
-        };
-        nixd.enable = true;
-        pyright = {
-          enable = true;
-          settings = {
-            typeCheckingMode = "strict";
-            disableOrganizeImports = false;
-            analysis = {
-              useLibraryCodeForTypes = true;
-              autoSearchPaths = true;
-              autoImportCompletions = true;
-            };
-          };
-        };
-        ruff.enable = true;
-        rust_analyzer = {
-          enable = true;
-          installRustc = false;
-          installCargo = false;
-        };
-        statix.enable = true;
-        tailwindcss.enable = true;
-        ts_ls.enable = true;
-        yamlls.enable = true;
       };
+      bashls.enable = true;
+      biome.enable = true;
+      clangd.enable = true;
+      docker_compose_language_service.enable = true;
+      dockerls.enable = true;
+      gopls.enable = true;
+      kotlin_language_server.enable = true;
+      lua_ls.enable = true;
+      marksman = {
+        enable = true;
+        settings = {
+          filetypes = ["md" "markdown"];
+        };
+      };
+      nixd.enable = true;
+      pyright = {
+        enable = true;
+        settings = {
+          typeCheckingMode = "strict";
+          disableOrganizeImports = false;
+          analysis = {
+            useLibraryCodeForTypes = true;
+            autoSearchPaths = true;
+            autoImportCompletions = true;
+          };
+        };
+      };
+      ruff.enable = true;
+      rust_analyzer = {
+        enable = true;
+        installRustc = false;
+        installCargo = false;
+      };
+      statix.enable = true;
+      tailwindcss.enable = true;
+      ts_ls.enable = true;
+      yamlls.enable = true;
     };
+  };
+
+  plugins = {
+    lspconfig.enable = true;
+    # lsp = {
+    #   enable = true;
+    #   keymaps.lspBuf = {
+    #     gD = "references";
+    #     gd = "definition";
+    #     gi = "implementation";
+    #     gt = "type_definition";
+    #     K = "hover";
+    #     "<leader>k" = "signature_help";
+    #     "<leader>F" = "format";
+    #   };
+    # };
     # lsp-format.enable = true;
     lsp-lines.enable = true;
     lspsaga = {
