4534c5c3d262636c5907b9867a62e6ec5e6715fd
body20002 <body20002.test@gmail.com>
2026-08-15 20:41:57 +0300

    Revert "autocmd: organize imports on save for ts/js"
    This reverts commit 2c2dc766130757aec3ea640c19ae060ed5902a01.

--- config/autocmd.nix
+++ config/autocmd.nix
@@ -6,7 +6,6 @@
     openMediaFiles.clear = true;
     saveFolds.clear = true;
     autoReload.clear = true;
-    organizeImports.clear = true;
   };
 
   autoCmd = [
@@ -15,18 +14,6 @@
       pattern = "*";
       command = "checktime";
       group = "autoReload";
-    }
-    {
-      event = ["BufWritePre"];
-      pattern = ["*.ts" "*.tsx" "*.js" "*.jsx" "*.mjs" "*.cjs"];
-      callback = {
-        __raw = ''
-          function()
-            pcall(vim.lsp.buf.code_action, {context = {only = {"source.organizeImports"}}})
-          end
-        '';
-        group = "organizeImports";
-      };
     }
     {
       event = ["BufWritePost"];
